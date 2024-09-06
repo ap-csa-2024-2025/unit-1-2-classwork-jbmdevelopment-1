66699d3512d9f11b7291bbeae9f9d8ee0537ec3e
jbmdevelopment <jbarreramartinez100@studen.hbuhsd.edu>
2024-09-06 11:10:55 -0700

    commit 2

--- problem2.java
+++ problem2.java
@@ -1,1 +1,18 @@
-// Create your class here; don't forget the main method
+
+public class problem2
+{
+    public static void main(String[] args) 
+    {
+        String name1 = "Sandy";
+        String name2 = "David";
+        String name3 = "Elaine";
+        String name4 = "Jesse";
+        String temp = name1;
+        name1 = name4;
+        name4 = temp;
+        System.out.print(name1);
+        System.out.print(name2);
+        System.out.print(name3);
+        System.out.print(name4);
+    }
+}
