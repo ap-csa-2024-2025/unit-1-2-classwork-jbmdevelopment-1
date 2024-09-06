09c02dd59746eaa7f03a500fcab0c7a3068315ad
jbmdevelopment <jbarreramartinez100@studen.hbuhsd.edu>
2024-09-06 11:03:20 -0700

    aaaa

--- problem1.java
+++ problem1.java
@@ -1,6 +1,9 @@
-// Create your class here; don't forget the main method
-public class problem1 {
-    public static void main(String[] args) {
-        System.out.println(1);
+
+public class problem1 
+{
+    public static void main(String[] args) 
+    {
+        String favFood = "pizza";
+        System.out.println("I like to eat " + favFood + " as well!");
     }
 }
